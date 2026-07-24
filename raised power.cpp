2b353feca89622ff6610a52883a997d330fb7701
joguaravind018 <joguaravind018@gmail.com>
2026-07-24 20:40:24 +0530

    Add files via upload

---
+++ raised power.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if((n&n-1)==0){
+        cout<<"it is raised power of 2";
+    }
+    else
+    cout<<"it is not raised power of 2";
+    
+}
